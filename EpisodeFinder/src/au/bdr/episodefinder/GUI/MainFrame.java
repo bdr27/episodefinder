7dd47d9cd09e6d4392df2811ba5aa3e8a7628c15
Brendan Russo <brendanrusso27@gmail.com>
2013-02-10 23:41:07 +1000

    Setting up the packages for the project. Have a main with an empty JFrame at the moment

---
+++ EpisodeFinder/src/au/bdr/episodefinder/GUI/MainFrame.java
@@ -0,0 +1,20 @@
+/*
+ * Main GUI of the program
+ */
+
+package au.bdr.episodefinder.GUI;
+
+import java.awt.Dimension;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Brendan Russo
+ */
+public class MainFrame extends JFrame{
+    
+    public MainFrame(){
+        this.setVisible(true);
+        this.setPreferredSize(new Dimension(800,600));
+    }
+}
